1461e1fcc30a36d952945b55b6059580800d4046
Sarah Gambles <sarahgambles14@gmail.com>
2020-09-04 09:05:56 -0600

    created HTML

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+// global variables
+var userFormEl = document.querySelector("#user-form");
+var containerEl = document.querySelector("#container");
+
+// create a form to use inputs
+
+// function to display date and time
+
+// varDisplayDateTime = function() {
+//   dateTime.textContent = "searchCity";
+// }
+
+// fetching
+
+    // add conditional
+
+// display functions
+
+// submit functions
+
+// localStorage features
+
+// call function
+
+// unpack json object
+
+// append city every time you add an entry
+
+// create issues for each step of the way
+
+// console log EVERYTHING
+
+// take it step by step creating the thing you want
